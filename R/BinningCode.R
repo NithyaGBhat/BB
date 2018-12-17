2635a8b29095781bc6e09debcb765f03aa614215
NithyaGBhat <45784483+NithyaGBhat@users.noreply.github.com>
2018-12-17 17:54:13 +0530

    Update BinningCode.R

--- R/BinningCode.R
+++ R/BinningCode.R
@@ -1,4 +1,4 @@
-BinningCode <- function(data_nDV) {
+BinningCode <- function(filename) {
 #data_nDV<-read.csv("C:/opencpuapp_ip/cleaned_data_without_DV.csv",stringsAsFactors = FALSE)
 data<-read.csv("C:/opencpuapp_ip/data_after_binning.csv",stringsAsFactors = FALSE)
 input.df <- data
@@ -10,7 +10,7 @@ names(data)[names(data)==target.var.name] <- "DV"
 ncol(data)
 
 library(dplyr)
-final_dataset <- inner_join(data, data_nDV, by = NULL, copy = FALSE)
+final_dataset <- inner_join(data, filename, by = NULL, copy = FALSE)
 final_dataset$DV <- NULL
 class(final_dataset)
 nrow(data)
