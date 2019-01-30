6f9eaba1503b01e3d2766bbd756541ac32749f04
NithyaGBhat <45784483+NithyaGBhat@users.noreply.github.com>
2019-01-30 17:55:09 +0530

    Update BinningCode.R

--- R/BinningCode.R
+++ R/BinningCode.R
@@ -1,22 +1,18 @@
 BinningCode <- function(filename) {
 #data_nDV<-read.csv("C:/opencpuapp_ip/cleaned_data_without_DV.csv",stringsAsFactors = FALSE)
-r_data<-read.csv("C:/opencpuapp_ip/data_after_binning.csv",stringsAsFactors = FALSE)
-r_data<-data.frame(r_data)
-input.df <- r_data
+data<-read.csv("C:/opencpuapp_ip/data_after_binning.csv",stringsAsFactors = FALSE)
+input.df <- data
 target.var.name <- "diabetes"
 
-
 #############################################
-install.packages("dplyr",repos = "http://cran.us.r-project.org")
-#library(dplyr)
-names(r_data)[names(r_data)==target.var.name] <- "DV"
-data_nDV<-data.frame(filename)
-names(data_nDV)[names(data_nDV)==target.var.name] <- "DV"
-  
-final_dataset <- dplyr::inner_join(r_data, data_nDV, by = NULL, copy = FALSE)
-final_dataset$DV <- NULL
+names(data)[names(data)==target.var.name] <- "DV"
 
-write.csv(data,"C:/opencpuapp_ip/f_dv.csv")
+data_nDV<-data.frame(data_nDV)
+final_dataset <- dplyr::inner_join(data, data_nDV, by = NULL, copy = FALSE)
+final_dataset$DV <- NULL
+class(final_dataset)
+
+write.csv(data,"f_dv.csv")
 write.csv(final_dataset,"C:/opencpuapp_ip/f_out.csv")
 
 }
